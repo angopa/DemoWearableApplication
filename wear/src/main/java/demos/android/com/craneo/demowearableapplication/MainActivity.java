02115f4e7aa45565d320ed2b69e540e3a618302c
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-13 16:54:09 -0400

    Commit before start the next example with the confirmation

--- wear/src/main/java/demos/android/com/craneo/demowearableapplication/MainActivity.java
+++ wear/src/main/java/demos/android/com/craneo/demowearableapplication/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.wearable.view.ActionPage;
 import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -17,13 +20,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager manager = getFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        CardFragment fragment = CardFragment.create(
-                getString(R.string.card_title),
-                getString(R.string.card_description),
-                R.drawable.landon_icon);
-        transaction.add(R.id.frame_layout, fragment);
-        transaction.commit();
+        ActionPage actionPage = (ActionPage) findViewById(R.id.action_page);
+        actionPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("MainActivity", "Button Click");
+            }
+        });
+
+        /**
+         * We use this lines for card  fragments
+         */
+//    FragmentManager manager = getFragmentManager();
+//    FragmentTransaction transaction = manager.beginTransaction();
+//    CardFragment fragment = CardFragment.create(
+//            getString(R.string.card_title),
+//            getString(R.string.card_description),
+//            R.drawable.landon_icon);
+//    transaction.add(R.id.frame_layout, fragment);
+//        transaction.commit();
+
+
     }
 }
+
+
+
