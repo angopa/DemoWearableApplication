17b3fbafb6e779337ca99c7915d890316ee65607
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-12 20:52:00 -0400

    Complete Wearable Application, its a very simple example of how the cradFragment works....

---
+++ wear/src/main/java/demos/android/com/craneo/demowearableapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package demos.android.com.craneo.demowearableapplication;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        CardFragment fragment = CardFragment.create(
+                getString(R.string.card_title),
+                getString(R.string.card_description));
+        transaction.add(R.id.frame_layout, fragment);
+        transaction.commit();
+    }
+}
