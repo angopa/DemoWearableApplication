03d55f2e2f37b75b8b3ac3d48c7eee1546391b02
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-14 10:55:51 -0400

    Commit before start the next example Acepting voice input.

--- wear/src/main/java/demos/android/com/craneo/demowearableapplication/AlwaysOnActivity.java
+++ wear/src/main/java/demos/android/com/craneo/demowearableapplication/AlwaysOnActivity.java
@@ -1,15 +1,21 @@
 package demos.android.com.craneo.demowearableapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class AlwaysOnActivity extends WearableActivity {
 
+    private static final int SPEECH_REQUEST_CODE = 1001;
     private TextView textView;
     private RelativeLayout layoutContainer;
 
@@ -22,11 +28,33 @@ public class AlwaysOnActivity extends WearableActivity {
 
         textView = (TextView) findViewById(R.id.textView);
         layoutContainer = (RelativeLayout) findViewById(R.id.layoutContainer);
-        
+
+        layoutContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySpeechRecognizer();
+            }
+        });
 
     }
 
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(intent, SPEECH_REQUEST_CODE );
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK){
+            List<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = result.get(0);
+
+            TextView tv = (TextView) findViewById(R.id.textView);
+            tv.setText("You said...\n"+spokenText);
+        }
+    }
 
     @Override
     public void onEnterAmbient(Bundle ambientDetails){
